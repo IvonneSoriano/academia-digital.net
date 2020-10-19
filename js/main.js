b089e736318a7f6b47eb2c149ab67f233bbb9547
Ivonne Sanchez <44445753+IvonneSoriano@users.noreply.github.com>
2020-10-19 10:42:16 -0600

    upload files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+    $('body').scrollspy({target: ".navbar"})
+
+    
+    AOS.init();
+    console.log("HOLA")
+    var altura = $('header').offset().top;
+
+    $(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('header').addClass('sticky');
+		} else {
+			$('header').removeClass('sticky');
+		}
+	});
+});
+
+function menuOpen(){
+    $('header').toggleClass("open");
+}
